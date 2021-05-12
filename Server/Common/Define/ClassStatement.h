017cf52f730c17a872177df1d815292c0d8f0d97
YueJianZhenG <646585122@qq.com>
2021-05-12 19:48:00 +0800

    修改编译错误

--- Server/Common/Define/ClassStatement.h
+++ Server/Common/Define/ClassStatement.h
@@ -1,14 +1,13 @@
 #pragma once
 #include<type_traits>
-namespace SoEasy
+
+template<typename T>
+struct TypeReflection
 {
-	template<typename T>
-	struct TypeReflection
-	{
-	public:
-		static constexpr bool Value = false;
-		static constexpr char * Name = nullptr;
-	};
+public:
+	static constexpr bool Value = false;
+	static constexpr char * Name = nullptr;
+};
 //#define TYPE_REFLECTION(type, base) template<> struct TypeReflection<type, base> { static constexpr char * Name = (char*)#type; static constexpr bool Value = true; typedef typename std::enable_if<(std::is_base_of<base, type>::value), type>::type Type;}
 
 	//template<typename T, typename Base>
@@ -22,7 +21,7 @@ namespace SoEasy
 		static constexpr size_t Size = sizeof(T);	\
 	}
 #define ChildType(type, base) typename std::enable_if<std::is_base_of<base,type>::value && TypeReflection<type>::Value, type>::type
-}
+
 
 namespace SoEasy
 {
