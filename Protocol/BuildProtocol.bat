d483351001b124cce9257b399f6d38cb920fc14f
lele <646585122@qq.com>
2021-06-26 15:44:15 +0800

    修改协议文件

--- Protocol/BuildProtocol.bat
+++ Protocol/BuildProtocol.bat
@@ -1,3 +1,4 @@
-protoc.exe "./Common.proto" --cpp_out=../Server/ServerData/Protocol
-protoc.exe "./ServerCommon.proto" --cpp_out=../Server/ServerData/Protocol
+protoc.exe "./c2s.proto" --cpp_out=../Server/ServerData/Protocol
+protoc.exe "./s2s.proto" --cpp_out=../Server/ServerData/Protocol
+protoc.exe "./com.proto" --cpp_out=../Server/ServerData/Protocol
 pause
