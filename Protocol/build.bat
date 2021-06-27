f6ad64c33b92bd6dbe7025681a30ce47fc145550
YueJianZhenG <64685122@qq.com>
2021-06-27 23:45:33 +0800

    修改协议

--- Protocol/build.bat
+++ Protocol/build.bat
@@ -1,3 +1,4 @@
+protoc.exe "./db.proto" --cpp_out=../Server/ServerData/Protocol
 protoc.exe "./c2s.proto" --cpp_out=../Server/ServerData/Protocol
 protoc.exe "./s2s.proto" --cpp_out=../Server/ServerData/Protocol
 protoc.exe "./com.proto" --cpp_out=../Server/ServerData/Protocol
