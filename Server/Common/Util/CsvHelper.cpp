0a70cd0af5a635452c0907f1f8f3f24f9a8c39e1
YueJianZhenG <64685122@qq.com>
2021-07-12 22:33:51 +0800

    1

---
+++ Server/Common/Util/CsvHelper.cpp
@@ -0,0 +1,109 @@
+﻿#include"CsvHelper.h"
+#include"FileHelper.h"
+#include"StringHelper.h"
+#include<fstream>
+namespace SoEasy
+{
+	void CsvLine::Add(const std::string & value)
+	{
+		this->mLineDatas.push_back(value);
+	}
+	bool CsvLine::GetData(size_t index, std::string & value)
+	{
+		if (this->mLineDatas.size() > 0 && index >= 0 && index < this->mLineDatas.size())
+		{
+			value = this->mLineDatas[index];
+			return true;
+		}
+		return false;
+	}
+	CsvLine & CsvLine::operator<<(const std::string & value)
+	{
+		this->mLineDatas.push_back(value);
+		return (*this);
+	}
+	
+}
+namespace SoEasy
+{
+	CsvFileReader::CsvFileReader(const std::string & path)
+		: mPath(path)
+	{
+		std::vector<std::string> tempArray;
+		std::vector<std::string> fileContent;
+		if (FileHelper::ReadTxtFile(path, fileContent))
+		{
+			for (size_t index = 0; index < fileContent.size(); index++)
+			{
+				const std::string & data = fileContent[index];
+				StringHelper::SplitString(data, "\t", tempArray);
+				if (tempArray.empty())
+				{
+					continue;
+				}
+				CsvLine * lineData = new CsvLine();
+				for (const std::string & value : tempArray)
+				{
+					lineData->Add(value);
+				}
+			}
+		}
+	}
+	CsvFileReader::~CsvFileReader()
+	{
+		for (size_t index = 0; index < this->mAllLines.size(); index++)
+		{
+			delete this->mAllLines[index];
+		}
+		this->mAllLines.clear();
+	}
+	CsvLine * CsvFileReader::GetLineData(size_t index)
+	{
+		if (this->mAllLines.size() > 0 && index >= 0 && index < this->mAllLines.size())
+		{
+			return this->mAllLines[index];
+		}
+		return nullptr;
+	}
+}
+
+namespace SoEasy
+{
+	CsvFileWriter::CsvFileWriter()
+	{
+
+	}
+
+	CsvFileWriter::~CsvFileWriter()
+	{
+		for (size_t index = 0; index < this->mAllLines.size(); index++)
+		{
+			delete this->mAllLines[index];
+		}
+		this->mAllLines.clear();
+	}
+
+	bool CsvFileWriter::Save(const std::string & path)
+	{
+		std::fstream fs(path, std::ios::out);
+		if (!fs.is_open())
+		{
+			return false;
+		}
+		for (size_t index = 0; index < this->mAllLines.size(); index++)
+		{
+			CsvLine * lineData = this->mAllLines[index];
+			for (size_t x = 0; x < lineData->GetCount(); x++)
+			{
+				std::string value;
+				if (lineData->GetData(x, value))
+				{
+					fs << value << "\t";
+				}
+			}
+			fs << "\n";
+		}
+		fs.close();
+		return true;
+	}
+}
---
+++ Server/Common/Util/CsvHelper.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include<string>
+#include<vector>
+namespace SoEasy
+{
+	class CsvLine
+	{
+	public:
+		void Add(const std::string & value);
+		bool GetData(size_t index, std::string & value);
+		const size_t GetCount() { return this->mLineDatas.size(); }
+	public:
+		CsvLine & operator <<(const std::string & value);
+	private:
+		std::vector<std::string> mLineDatas;
+	};
+}
+
+namespace SoEasy
+{
+	class CsvFileReader
+	{
+	public:
+		CsvFileReader(const std::string & path);
+		~CsvFileReader();
+	public:
+		const size_t GetCount() { return this->mAllLines.size(); }
+		CsvLine * GetLineData(size_t index);
+	private:
+		const std::string & mPath;
+		std::vector<CsvLine *> mAllLines;
+	};
+}
+
+namespace SoEasy
+{
+	class CsvFileWriter
+	{
+	public:
+		CsvFileWriter();
+		~CsvFileWriter();
+	public:
+		void Add(CsvLine * lineData);
+		bool Save(const std::string & path);
+	private:
+		std::vector<CsvLine *> mAllLines;
+	};
+}
