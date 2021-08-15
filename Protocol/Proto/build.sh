424f6aabd70b04199823d7129b19e4e95ec1fce3
YueJianZhenG <646585122@qq.com>
2021-08-15 12:02:46 +0800

    重构项目

---
+++ Protocol/Proto/build.sh
@@ -0,0 +1,3 @@
+protoc "./c2s.proto" --cpp_out=../Server/ServerData/Protocol
+protoc "./s2s.proto" --cpp_out=../Server/ServerData/Protocol
+protoc "./com.proto" --cpp_out=../Server/ServerData/Protocol
