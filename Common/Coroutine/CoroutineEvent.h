a8aa7bd2ec1888991588e8820b481e35ce6eed1f
YueJianZhenG <646585122@qq.com>
2021-08-22 11:09:32 +0800

    删除废弃代码

--- Common/Coroutine/CoroutineEvent.h
+++ Common/Coroutine/CoroutineEvent.h
@@ -21,54 +21,6 @@
 	#define STACK_SIZE 1024 * 1024
 #endif
 
-namespace Sentry
-{
-    class CoroutineManager;
-
-    typedef std::function<void()> CoroutineAction;
-
-    class CoroutineEvent
-    {
-    public:
-        CoroutineEvent(CoroutineManager *mgr, long long id);
-
-    public:
-        virtual bool Invoke() = 0;
-
-    public:
-        long long GetBindCoroutineID() { return mBindCoroutineId; }
-
-    protected:
-        CoroutineManager *mCorScheduler;
-    private:
-        long long mBindCoroutineId;
-    };
-
-    class CorSleepEvent : public CoroutineEvent
-    {
-    public:
-        CorSleepEvent(CoroutineManager *, long long id, long long ms);
-
-    public:
-        bool Invoke() override;
-
-    private:
-        long long mNextInvokeTime;
-    };
-
-    class CorNextFrameEvent : public CoroutineEvent
-    {
-    public:
-        CorNextFrameEvent(CoroutineManager *, long long id, int count);
-
-    public:
-        bool Invoke() override;
-
-    private:
-        int mCount;
-        int mMaxCount;
-    };
-}
 
 namespace Sentry
 {
