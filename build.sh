1a9769a9c598ee09a689d3d3866c9183a3f1bfd2
YueJianZhenG <646585122@qq.com>
2021-08-27 00:15:34 +0800

    增加批处理命令

---
+++ build.sh
@@ -0,0 +1,14 @@
+  
+echo "start build server..."
+find ./ -name CMakeFiles | xargs rm -r
+find ./ -name cmake_install.cmake -delete
+find ./ -name CMakeCache.txt -delete
+cmake ./CMakeLists.txt
+
+make -j2
+
+find ./ -name CMakeFiles | xargs rm -r
+find ./ -name cmake_install.cmake -delete
+find ./ -name CMakeCache.txt -delete
+
+echo "build server successful..."
