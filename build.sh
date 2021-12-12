ebf137a44e2c19da45face583c8763ce2d330292
yjz <646585122@qq.com>
2021-12-12 13:02:16 +0800

    修改协程汇编文件

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ find ./ -name cmake_install.cmake -delete
 find ./ -name CMakeCache.txt -delete
 cmake ./CMakeLists.txt
 
-make -j4
+make -j8
 
 find ./ -name CMakeFiles | xargs rm -r
 find ./ -name cmake_install.cmake -delete
