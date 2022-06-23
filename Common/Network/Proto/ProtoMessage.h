1ad62a4ae3dd5207427026b0c1e9c2ed3d2d32f6
YueJianZhenG <646585122@qq.com>
2022-06-23 21:29:30 +0800

    在lua中生成protobuf消息

--- Common/Network/Proto/ProtoMessage.h
+++ Common/Network/Proto/ProtoMessage.h
@@ -20,5 +20,40 @@ namespace Tcp
 		void Write(std::ostream & os, const std::string & value);
 		void Write(std::ostream & os, const char * str, size_t size);
 	};
+
+	class BinProtoMessage : public ProtoMessage
+	{
+	public:
+		BinProtoMessage(const std::string & str);
+		BinProtoMessage(const char * str, size_t size);
+		~BinProtoMessage();
+	public:
+		int Serailize(std::ostream & os); //返回剩余要发送的字节数
+	private:
+		size_t size;
+		char * mBuffer;
+	};
+	BinProtoMessage::BinProtoMessage(const std::string& str)
+	{
+		this->mBuffer = new char[str.size()];
+		memcpy(this->mBuffer, str.c_str(), str.size());
+	}
+
+	BinProtoMessage::~BinProtoMessage()
+	{
+		delete []mBuffer;
+	}
+
+	BinProtoMessage::BinProtoMessage(const char* str, size_t size)
+	{
+		memcpy(this->mBuffer, str, size);
+	}
+
+	int BinProtoMessage::Serailize(std::ostream& os)
+	{
+		os.write(this->mBuffer, size);
+		return 0;
+	}
+
 }
 #endif //_PROTOMESSAGE_H_
