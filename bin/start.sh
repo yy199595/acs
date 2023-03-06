5bf334e5b100b6083f8a1df9f20d95ac37bae258
yjz <646585122@qq.com>
2023-03-06 20:29:25 +0800

    修改多进程启动脚本

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,7 @@
-screen -dms yjz.tran ./app ./config/server.json TranServer
+screen -dms yjz.registry ./app ./config/server.json RegistryServer
 screen -dms yjz.data ./app ./config/server.json DataServer
 screen -dms yjz.http ./app ./config/server.json HttpServer
 screen -dms yjz.chat ./app ./config/server.json ChatServer
 screen -dms yjz.gate ./app ./config/server.json GateServer
+screen -dms yjz.log ./app ./config/server.json LogServer
 ps -aux|grep yjz
