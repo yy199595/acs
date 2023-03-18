16b9288f788c8536b71137e098865ff2ff31aaa1
YueJianZhenG <646585122@qq.com>
2023-03-19 02:02:02 +0800

    修改启动脚本

--- bin/start.bat
+++ bin/start.bat
@@ -4,4 +4,3 @@ start ./Debug/app.exe ./config/server.json DataServer
 start ./Debug/app.exe ./config/server.json GateServer
 start ./Debug/app.exe ./config/server.json ChatServer
 start ./Debug/app.exe ./config/server.json HttpServer
-start ./Debug/app.exe ./config/server.json LogServer
