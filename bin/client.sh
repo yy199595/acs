d4d6908ac36456a9053ee670f1695c2dcbe9da6f
yjz199595 <646585122@qq.com>
2024-11-25 17:05:05 +0800

    测试

--- bin/client.sh
+++ bin/client.sh
@@ -3,4 +3,4 @@ ulimit -c unlimited
 export COREDUMP_FILENAME=./
 sysctl -w kernel.core_pattern="core.%t"
 
-/app --rpc=7788 --http=8088 --gate=7789 --lua=Client
+./app --rpc=7788 --http=8088 --gate=7789 --lua=Client
